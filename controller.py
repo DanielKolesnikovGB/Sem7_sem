8d947ec78db1e74a80f9c554bda566976067f450
Daniel <rabinovich130@gmail.com>
2022-12-02 02:16:10 +0700

    content was added to file

--- controller.py
+++ controller.py
@@ -1,6 +1,9 @@
 import view
 import rational_numbers
 import rational_numbers_init
+import logger as log
 
 def start ():
     view.result(rational_numbers.calc(rational_numbers_init.init()))
+    log.action_notes(rational_numbers_init.init(),\
+        rational_numbers.calc(rational_numbers_init.init()))
